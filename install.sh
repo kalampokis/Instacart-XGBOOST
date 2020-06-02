abfe3a8273763137e4eb08d29bd7aec9feea9844
Evangelos Kalampokis <evangkal@gmail.com>
2020-06-02 18:47:34 +0300

    install

---
+++ install.sh
@@ -0,0 +1,25 @@
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+sudo apt-get update
+sudo apt-get -y install cuda
+
+sudo apt-get install python-pip
+
+git config --global credential.helper gcloud.sh
+
+git clone https://github.com/kalampokis/Instacart-XGBOOST.git
+
+pip install sklearn
+pip install pandas
+pip install Kaggle
+
+pip install xgboost 
+
+mkdir -p ~/.kaggle
+cp kaggle.json ~/.kaggle/
+
+cd Instacart-XGBOOST
+
+python instacart-xgboost-gridsearch.py
